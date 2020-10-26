eaddbc78aeee25b3cba6cad12bc67a630cc74854
ismynr <gekkai7is.my@gmail.com>
2020-10-26 22:37:25 +0700

    kirim data dengan bundle

--- app/src/main/java/com/ismynr/mynavigation/HomeFragment.kt
+++ app/src/main/java/com/ismynr/mynavigation/HomeFragment.kt
@@ -19,6 +19,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+@Suppress("NAME_SHADOWING")
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
