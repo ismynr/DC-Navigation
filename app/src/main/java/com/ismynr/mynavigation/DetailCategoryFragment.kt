e34f73eca0a42171f9cc20e87ac93235867e4fc8
ismynr <gekkai7is.my@gmail.com>
2020-10-27 08:19:38 +0700

    Pop Up Behaviour

--- app/src/main/java/com/ismynr/mynavigation/DetailCategoryFragment.kt
+++ app/src/main/java/com/ismynr/mynavigation/DetailCategoryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_detail_category.*
 
 class DetailCategoryFragment : Fragment() {
@@ -33,5 +34,9 @@ class DetailCategoryFragment : Fragment() {
 
         tv_category_name.setText(dataName)
         tv_category_description.setText("Stock : $dataDescription")
+
+        btn_profile.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
     }
 }
